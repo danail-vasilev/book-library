d532462a6f22c84aaef126a199abf56be44ee1e2
Danail Vasilev <massaro@mail.bg>
2023-02-12 14:28:19 +0200

    Refactored book-lib code; Added open zeppelin lib;

--- contracts/BookLibrary.sol
+++ contracts/BookLibrary.sol
@@ -1,75 +1,141 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
-import "./Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BookLibrary is Ownable {
-    // Quantiy of books must be visible to users
-    mapping(uint => uint) public bookIdToQuantity;
-    mapping(address => uint[]) private borrowerToBookIds;
-    address[] allBorrowers;
-
-    function addBook(uint _id, uint quantity) external onlyOwner {
-        bookIdToQuantity[_id] += quantity;
+    using StringUtil for string;
+    struct Book {
+        string title;
+        uint copies;
+        address[] bookBorrowedAddresses; // TODO: Change to addresses that have ever borrowed a book
     }
 
-    function listBorrowers() external view returns (address[] memory) {
-        return allBorrowers;
+    // It's more cheaper to make a state variable public rather than creating a getter function
+    // Solidity already creates a getter for public vars
+    // OpenZeppelin, however, creates such public getters in order to avoid state variables override in inheritance
+    bytes32[] public bookKey;
+
+    mapping(bytes32 => Book) public books;
+
+    // Use nested mapping to avoid loops.
+    // bytes32 is more gas efficient than uint256 for storing book ids
+    mapping(address => mapping(bytes32 => bool)) private userToBorrowedBooks;
+
+    event BookAdded(
+        string title,
+        address owner,
+        uint256 addedCopies,
+        uint256 allCopies
+    );
+    event BookBorrowed(string title, address borrower, uint256 leftCopies);
+    event BookReturned(string title, address borrower, uint256 leftCopies);
+
+    function addBook(
+        string calldata _title,
+        uint256 _copies
+    ) external onlyOwner validTitle(_title) {
+        require(_copies > 0, "Invalid number of copies");
+        bytes32 bookId = _title.toBytes32();
+
+        Book storage book = books[bookId];
+        // Initial book adding:
+        //  * update title of the struct and push book id.
+        //  * struct's default values: copies = 0, bookBorrowedAddresses = [];
+        if (book.title.isEmpty()) {
+            book.title = _title;
+            bookKey.push(bookId);
+        }
+        // Update copies of the book. If book is initially added copies defaults to 0.
+        book.copies += _copies;
+        emit BookAdded(_title, msg.sender, _copies, book.copies);
     }
 
-    function borrowBook(uint _bookId) external bookAvailable(_bookId) {
+    // Modifiers for valid title and book existence can be added
+    function listBookBorrowers(
+        string calldata _title
+    ) external view returns (address[] memory) {
+        bytes32 bookId = _title.toBytes32();
+        return books[bookId].bookBorrowedAddresses;
+    }
+
+    function borrowBook(
+        string calldata _title
+    ) external validTitle(_title) availableBook(_title) {
+        bytes32 bookId = _title.toBytes32();
+
+        mapping(bytes32 => bool) storage borrowedBooks = userToBorrowedBooks[
+            msg.sender
+        ];
+        require(!borrowedBooks[bookId], "You have already borrowed this book!");
+
+        books[bookId].copies -= 1;
+        books[bookId].bookBorrowedAddresses.push(msg.sender);
+
+        borrowedBooks[bookId] = true;
+        emit BookBorrowed(_title, msg.sender, books[bookId].copies);
+    }
+
+    modifier availableBook(string memory _title) {
+        bytes32 id = _title.toBytes32();
         require(
-            !isBookBorrowed(borrowerToBookIds[msg.sender], _bookId),
-            "You have already borrowed this book!"
+            books[id].copies > 0,
+            "There are no available books at the moment"
         );
-        // Remove book from the lib
-        bookIdToQuantity[_bookId]--;
-        // Add book to the borrower
-        borrowerToBookIds[msg.sender].push(_bookId);
-        // TODO check for duplicate before adding;
-        // TODO check for a better solution/struct something like set.
-        allBorrowers.push(msg.sender);
-    }
-
-    modifier bookAvailable(uint _id) {
-        require(bookIdToQuantity[_id] > 0, "This book is not available");
         _;
     }
 
-    function isBookBorrowed(
-        uint[] memory bookIds,
-        uint _bookId
-    ) private pure returns (bool) {
-        // TODO check whether NPE is required if bookIds is empty for Solidity
-        for (uint i = 0; i < bookIds.length; i++) {
-            if (bookIds[i] == _bookId) {
-                return true;
-            }
-        }
-        return false;
-    }
+    function returnBook(
+        string calldata _title
+    ) external validTitle(_title) borrowedBook(_title) {
+        bytes32 bookId = _title.toBytes32();
 
-    function returnBook(uint _bookId) external {
-        require(
-            isBookBorrowed(borrowerToBookIds[msg.sender], _bookId),
-            "You haven't borrowed this book!"
-        );
         // Add back the book to the lib
-        bookIdToQuantity[_bookId]++;
+        books[bookId].copies += 1;
 
         // Remove the book from the borrower
-        uint[] storage borrowedBooks = borrowerToBookIds[msg.sender];
+        userToBorrowedBooks[msg.sender][bookId] = false;
 
-        // TODO: Check whether it's fine to create a new uint[] array and assign it and why this approach hangs the transaction ?
+        address[] storage bookBorrowedAddresses = books[bookId]
+            .bookBorrowedAddresses;
 
         // Unordered array deletion
+        // TODO: Make bookBorrowedAddresses historical and remove that
         uint idxToDelete;
-        for (uint i = 0; i < borrowedBooks.length; i++) {
-            if (borrowedBooks[i] == _bookId) {
+        for (uint i = 0; i < bookBorrowedAddresses.length; i++) {
+            if (bookBorrowedAddresses[i] == msg.sender) {
                 idxToDelete = i;
                 break;
             }
         }
-        delete borrowedBooks[idxToDelete];
-        borrowedBooks.pop();
+        delete bookBorrowedAddresses[idxToDelete];
+        bookBorrowedAddresses.pop();
+        emit BookReturned(_title, msg.sender, books[bookId].copies);
+    }
+
+    // TODO: Best practice for params data location in modifiers ?
+    //  * both calldata and memory compiles, no gas change
+    //  * Probably memory because modifiers are internal
+    modifier validTitle(string memory _title) {
+        require(!_title.isEmpty(), "Book title is not valid");
+        _;
+    }
+
+    modifier borrowedBook(string memory _title) {
+        require(
+            userToBorrowedBooks[msg.sender][_title.toBytes32()],
+            "You haven't borrowed this book"
+        );
+        _;
+    }
+}
+
+library StringUtil {
+    // Compiles also with calldata + public/external; No change is gas
+    function toBytes32(string memory _str) internal pure returns (bytes32) {
+        return bytes32(bytes(_str));
+    }
+
+    function isEmpty(string memory _str) internal pure returns (bool) {
+        return bytes(_str).length == 0;
     }
 }
